d8b127a89a36cd389bfc527ad6f4f176adcf5b98
Deferrent <jsam@DESKTOP-M13O9DR.localdomain>
2023-01-03 17:19:34 -0800

    Updated

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = "string"
+}
+
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
   }
+}
+
+provider "aws"{
+  aws_access_key = TF_VAR_AWS_ACCESS_KEY_ID
+  region = "us-west-1"
 }
 
 resource "aws_instance" "" {
