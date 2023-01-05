c6f9995d575c5efe8b519f04f93b8de8a5f08036
Deferrent <jsam@DESKTOP-M13O9DR.localdomain>
2023-01-05 13:12:42 -0800

    Establish Terraform in CI pipeline

--- main.tf
+++ main.tf
@@ -1,27 +1,25 @@
-variable "AWS_ACCESS_KEY_ID" {}
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
+# use ubuntu 20 AMI for EC2 instance
+data "aws_ami" "ubuntu" {
+    most_recent = true
+filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
     }
-  }
+filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+owners = ["099720109477"] # Canonical
 }
 
-provider "aws"{
-  aws_access_key = TF_VAR_AWS_ACCESS_KEY_ID
-  region = "us-west-1"
+provider "aws" {
+  region  = "us-west-1"
 }
-
-resource "aws_instance" "example" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+tags = {
+    Name = var.ec2_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "network_interface_id" {
+variable "ec2_name" {
   type = string
-  default = "network_id_from_aws"
-}
-
-variable "ami" {
-    type = string
-    default = "ami-005e54dee72cc1d00"
-}
-
-variable "instance_type" {
-    type = string
-    default = "t2.micro"
 }
