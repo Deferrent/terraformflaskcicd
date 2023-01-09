1afab13bb1046e67daaf7074cf2b016d8698d6ac
Deferrent <jsam@DESKTOP-M13O9DR.localdomain>
2023-01-08 19:42:26 -0800

    Adding nomenclature of s3 and ec2 to variables tf file

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "testing-s3-with-terraform-joseph-sam"
+   bucket = "${var.s3_name}"
    acl = "private"
    versioning {
       enabled = true
--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
 variable "ec2_name" {
-  default = "Terraform Flask Server"
+  default = "Terraform Flask Server - Joseph Sam"
   type = string
 }
 
 variable "s3_name" {
+  default = "Terraform Flask Bucket - Joseph Sam"
   type = string
 }
