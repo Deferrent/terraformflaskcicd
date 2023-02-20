c741d62ec945d0512003698173792b994ace8446
Deferrent <jsam@DESKTOP-M13O9DR.localdomain>
2023-02-19 18:08:01 -0800

    Updated

--- TerraformFiles/variables.tf
+++ TerraformFiles/variables.tf
@@ -5,7 +5,7 @@ variable "beanstalkappenv" {
   default = "terraformflaskenv"
 }
 variable "solution_stack_name" {
-  default = "64bit Amazon Linux 2 v3.2.0 running Python 3.8"
+  default = "64bit Amazon Linux 2 v3.4.4 running Python 3.7"
 }
 variable "tier" {
   default = "WebServer"
